08d3beac02d45f117e8a1fd333551bbe0c78186f
GeekGhc <ghcgavin@sina.com>
2017-03-04 20:45:43 +0800

    ikonw api

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar');//保存用户头像
+            $table->string('confirm_code',64);//邮箱确认激活code
+            $table->smallInteger('is_confirmed')->default(0);//判断用户呢是否已经激活他的邮箱
+            $table->integer('followers_count')->default(0);
+            $table->integer('followings_count')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
